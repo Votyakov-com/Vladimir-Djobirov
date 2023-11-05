712eb7383e6f184f03a6ab13180dbb620aa4b496
Vladimir_Djobirov <djobirov05@mail.ru>
2023-11-05 20:52:34 +0500

    Фреймворк Flask. Практика

--- Flask_practice_framework/task_3.py
+++ Flask_practice_framework/task_3.py
@@ -23,4 +23,13 @@ def tasks_get():
     return (tasks, 200, {"content-type": "application/json"})
 
 
+@app.delete("/tasks/<int:id>")
+def tasks_delete(id):
+    if len(tasks) > 0:
+        tasks.pop(id - 1)
+        return f"Запись под номером {id} успешно удалена!"
+    else:
+        return "В вашей системе нету активных задач!"
+
+
 app.run()
